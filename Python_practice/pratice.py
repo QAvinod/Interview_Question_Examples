941a255f755d37e18de998e257b1667eda890a17
Vinod Kumar Eraganaboina <Vinod-E>
2021-04-05 19:08:56 +0530

    first commit2

---
+++ Python_practice/pratice.py
@@ -0,0 +1,9 @@
+def v():
+    a = [1, 2]
+    c = sum(a)
+    print(c)
+    return c
+
+
+if v() == 3:
+    print('yes')
--- Selenium_pratice/InterviewQuestions.py
+++ Selenium_pratice/InterviewQuestions.py
@@ -1,10 +1,22 @@
 """
+Testing
+--------
+1. RTM /SDLC / STLC / Testplan / Defect life cycle
+2. E2E testing
+3. smoke /sanity / functional / regression testing
+
 selenium
 --------
 1. what is the .get() and .navigate()
 2. types of wait
 3. With out jquery how you will Enter the Keys
 4. How to switch new tab
+5. xpath types (answer: relevant(/) , absolute (//) path)
+6. webdriver is a tool ?
+7. How many ways to approach dropdown select.
+8. What is Synchronization? Handling Synchronization in Selenium WebDriver
+9. How to handle browser pop-up
+10. How to handle window pop-up
 
 Python
 ------
@@ -12,9 +24,12 @@ Python
 2. How to handle error
 3. Type of functions in python
 4. How many data types. what are they
-5. What are Pythons collections
+5. What are Python collections
 6. add function with parametrized call
 7. how will return value from function
+8. What is the template in API automation (answer: URL/HEADERS/BODY)
+9. HTTP Request Types
+
 
 SQL
 ---
